2b25e4dc8cc6f7d2cfeca1f20657a87507c0488a
HolyLoki <forwoermind@gmail.com>
2019-11-08 20:20:23 +0600

    new animals

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,14 @@
 package com.company;
+
 public class Main {
 
     public static void main(String[] args) {
+        Animal animal = new Animal(
+                "yellow",
+                4,
+                true
+        );
+
 
     }
 }
